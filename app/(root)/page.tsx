551daabd08dfac71ad55ffaa87ec5ef4015e3c9b
mfleitas75 <m.fleitas33064@gmail.com>
2024-10-02 20:11:33 -0400

    Added Latest Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
-
-export default function Home() {
-  return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
-  );
-}
+import ProductList from '@/components/shared/product/product-list'
+   import data from '@/lib/data'
+   export default function Home() {
+     return (
+       <div className="space-y-8">
+         <h2 className="h2-bold">Latest Products</h2>
+         <ProductList data={data.products} />
+       </div>
+     )
+   }
